01dcff0c5764fcd1e5ee679c902a17a778b392ca
sltnsrh <lifenjoy8888@gmail.com>
2022-05-28 17:11:07 +0300

    redundant code was deleted

--- src/main/java/com/footballmanager/model/Team.java
+++ src/main/java/com/footballmanager/model/Team.java
@@ -2,7 +2,6 @@ package com.footballmanager.model;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -32,25 +31,4 @@ public class Team {
     private List<Player> players;
     private BigDecimal count;
     private BigDecimal commission;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Team team = (Team) o;
-        return id.equals(team.id)
-                && name.equals(team.name)
-                && players.equals(team.players)
-                && count.equals(team.count)
-                && commission.equals(team.commission);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, players, count, commission);
-    }
 }
--- src/main/java/com/footballmanager/service/mapper/TeamMapper.java
+++ src/main/java/com/footballmanager/service/mapper/TeamMapper.java
@@ -5,17 +5,15 @@ import com.footballmanager.dto.response.TeamResponseDto;
 import com.footballmanager.model.Player;
 import com.footballmanager.model.Team;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class TeamMapper implements ResponseDtoMapper<Team, TeamResponseDto>,
         RequestDtoMapper<Team, TeamRequestDto> {
     private final ModelMapper modelMapper;
-
-    public TeamMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
 
     @Override
     public Team mapToModel(TeamRequestDto dto) {
